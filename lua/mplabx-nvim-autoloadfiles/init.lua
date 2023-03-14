3351d93580df48bb8c51701808e7a53ebd2a421d
Mirko <mirkopoj@hotmail.com>
2023-03-14 10:58:30 -0300

    Sin auto cmd directo

--- lua/mplabx-nvim-autoloadfiles/init.lua
+++ lua/mplabx-nvim-autoloadfiles/init.lua
@@ -99,8 +99,6 @@ function Add_file_to_mplab_project()
 
 end
 
---vim.api.nvim_create_autocmd("BufferNew",  function() Add_file_to_mplab_project() end)
---vim.cmd "autocmd BufferNew lua CodeRunner()"
 local new_mplab_file = vim.api.nvim_create_augroup('new_mplab_file', {clear = true})
 vim.api.nvim_create_autocmd({"BufEnter", "BufNew"}, {
   group = "new_mplab_file",
